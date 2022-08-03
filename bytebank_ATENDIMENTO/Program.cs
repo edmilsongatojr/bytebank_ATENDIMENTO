98c93a8bbda5f87c11b54d4d443f4db62451208b
Edmilson Gato Junior <edygatojr@live.com>
2022-08-03 14:37:29 -0300

    TestaMediana. Exemplo básico para usar a superclasse array.

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
 //TestaArrayInt();
-TestaBuscarPalavra();
+//TestaBuscarPalavra();
 
 void TestaArrayInt()
 {
@@ -48,8 +48,41 @@ void TestaBuscarPalavra()
         if (palavra.Equals(busca))
         {
             Console.WriteLine($"Palavra Encontrada: {busca}");
+            break;
         }
-        else
-            Console.WriteLine($"Palavra não encontrada!");
+        
     }
+}
+
+
+
+Array amostra = Array.CreateInstance(typeof(double),5);
+
+amostra.SetValue(5.9,0);
+amostra.SetValue(1.8,1);
+amostra.SetValue(7.1,2);
+amostra.SetValue(10,3);
+amostra.SetValue(2.35,4);
+
+//[5,9] [1,8] [7,1] [10] [6,9]
+
+TestaMediana(amostra);
+
+void TestaMediana(Array array)
+{
+
+    if ((array == null) || (array.Length==0))
+    {
+        Console.WriteLine($"Array para cálculo da mediana está vazio ou nulo");    
+    }
+
+    double[] numerosOrdenados = (double [])array.Clone();
+    Array.Sort(numerosOrdenados);
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] : (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+
+    Console.WriteLine($"Com base na amostra a mediana = {mediana}");
 }
