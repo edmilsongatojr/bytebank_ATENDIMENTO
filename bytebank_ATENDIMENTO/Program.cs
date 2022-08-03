14ae30ad531844c3f2c666d88e904da3c7e7920c
Edmilson Gato Junior <edygatojr@live.com>
2022-08-03 13:54:53 -0300

    Implementando Array de Idades, total e média.

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,2 +1,30 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
+TestaArrayInt();
+
+void TestaArrayInt()
+{
+    int[] idades = new int[5];
+
+    idades[0] = 100;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
+
+    Console.WriteLine($"Tamanho ARRAY: {idades.Length}");
+
+    int acumulador = 0;
+
+
+    for (int i = 0; i < idades.Length; i++)
+    {
+        int idade = idades[i];
+        Console.WriteLine($"Indice [{i}] = {idade} ");
+        acumulador += idade;
+    }
+
+    int media = acumulador / idades.Length;
+    Console.WriteLine($"Media de idades = {media}");
+}
+
